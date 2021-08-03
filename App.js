30fcf37dfaf6981526cc734c931dc6e49915b3c0
Swarnaparvathi Gunasekarasivam <54331359+swarnaparvathi@users.noreply.github.com>
2021-08-03 15:43:54 +0530

    React quiz

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
+import React from 'react';
 import './App.css';
-import avatar from './assets/Signup.jpg';
-import { Signup } from './components/Signup';
-import { Confirmation } from './components/Confirmation';
+import Quiz from './components/QuizMain';
+
 function App() {
   return (
-    <div className="container mt-3">
-      <div className="row">
-        <div className="col-md-5">
-          <Signup />
-        </div>
-        <div className="col-md-7 my-auto">
-          <img className="img-fluid w-100" src={avatar} alt=""/>
-        </div>
-
-      </div>
+    <div className="App">
+      <Quiz />
     </div>
   );
 }
